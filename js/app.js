b78641ae54e1bd1865bf10b7f3ebf1848f743da5
Jay Lowack <lowjack@MacBook-Pro.lan>
2016-07-23 10:04:43 -0500

    updates

--- js/app.js
+++ js/app.js
@@ -1,12 +1,42 @@
-function AppViewModel() {
-  this.searchLoc = ko.observable("76262");
+var JSONLocationData = [{"geometry":{"location":{"lat":33.01193849999999,"lng":-97.21863160000001}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"1a59de1a05c550cef9b35ce049b5f825f70286ec","name":"Yogurt Story","place_id":"ChIJEYfKVFPQTYYR8hMZX3-M4xw"},{"geometry":{"location":{"lat":32.9979562,"lng":-97.22985180000001}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"c25d9d99c0cdc38685ca37199a7ca3ba7044925e","name":"Hey Sugar","place_id":"ChIJQWU1yfvQTYYRK84o-wD4Hig"},{"geometry":{"location":{"lat":32.93989800000001,"lng":-97.1813509}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"7bd64954fec28c70016721e90ae0c6f0125add2c","name":"Ice Cream Place","place_id":"ChIJ3a45JKDWTYYRqbmfS_90qVk"},{"geometry":{"location":{"lat":32.93869789999999,"lng":-97.18321189999999}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"7e679bd8772bcb20b9f7905260e51d926b5a322b","name":"Baskin Robbins","place_id":"ChIJjclKRJ_WTYYR05b_SpK9d54"},{"geometry":{"location":{"lat":32.9350027,"lng":-97.2195455}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png","id":"00117d97d1e9c3bb53a724d84557f31488de4921","name":"Freddy's Frozen Custard & Steakburgers","place_id":"ChIJwVYdqOHWTYYRsBd_WKlrnUM"},{"geometry":{"location":{"lat":32.9424235,"lng":-97.13033179999999}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"e9c714f7bc4cf08232fcb2fda1b2419ecd8b44e9","name":"Milwaukee Joe's Gourmet Ice","place_id":"ChIJQ0Uva-3UTYYRQUt5xl33bKo"},{"geometry":{"location":{"lat":32.9415885,"lng":-97.15578350000001}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"81e2db512508156e512946e9c101615b6d3490d6","name":"Rita's","place_id":"ChIJRVWf-z_UTYYRSRSuCQGfM9g"},{"geometry":{"location":{"lat":32.9777577,"lng":-97.2746367}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png","id":"022eecfa9c397cd57a68c055abe7da97712a40f6","name":"Mister Softee Texas","place_id":"ChIJEWzVcg3aTYYRCLtpqGz_zaI"},{"geometry":{"location":{"lat":32.9078088,"lng":-97.18984759999999}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png","id":"8f0cba234442845acee7bf44d3949aaa866ac445","name":"Yogurtville","place_id":"ChIJETuEzXrWTYYRqUPnFJzzSAE"},{"geometry":{"location":{"lat":33.0744424,"lng":-97.1294996}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"c0a9b6973969687863d0a63c1b2532bbd55abbcc","name":"Marble Slab Creamery","place_id":"ChIJRb25nnPNTYYRazbBaAhAkZQ"},{"geometry":{"location":{"lat":32.9122515,"lng":-97.3323243}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"94e17986be126ba317bfe8d454931f9ceae4002b","name":"Blue Bell Creameries","place_id":"ChIJgcWylSHZTYYRAfloxGC4cAY"},{"geometry":{"location":{"lat":32.91195440000001,"lng":-97.3133799}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"7aa61040ffe9be0704b04635a949ea01997c9f30","name":"Cold Stone Creamery","place_id":"ChIJHSGKeQHZTYYR4JKadQ3KhlQ"},{"geometry":{"location":{"lat":32.878856,"lng":-97.28889599999999}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"b517528a55e77f80bd5a7022f0b95a0c87e1d824","name":"Woolley's Frozen Custard (Beach St.)","place_id":"ChIJV9BlnW3YTYYRVmU14nLRlNM"},{"geometry":{"location":{"lat":33.0745517,"lng":-97.08547519999999}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"3ad0fa806d49cd8ee4174cfeebf5db06df132444","name":"Ben & Jerry's","place_id":"ChIJR_vFybYyTIYRarGSvubbYbU"},{"geometry":{"location":{"lat":32.8750819,"lng":-97.16326889999999}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"dea5af18570caa7296995a11c703ac733832e996","name":"Braum's Ice Cream & Burger Restaurant","place_id":"ChIJDZdnDPbVTYYRIwv37Pu4XWQ"},{"geometry":{"location":{"lat":33.013295,"lng":-97.072639}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png","id":"58425df763f597a8eb3ac9fc0979c72f4b94ec72","name":"Culver's","place_id":"ChIJkza29N8sTIYRbPbaInLILG4"},{"geometry":{"location":{"lat":32.8535563,"lng":-97.1880561}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"7ca33a74720eac9ce3785d20dd2c03314b132452","name":"Sub Zero Ice Cream & Yogurt","place_id":"ChIJyWjEEbd4ToYRFq2Muxgnclc"},{"geometry":{"location":{"lat":32.89414199999999,"lng":-97.2644765}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"3d2646d328be9df0e869013471744c5b3907ea23","name":"Bahama Buck's Fort Worth - Keller, TX","place_id":"ChIJT2ZwoCDYTYYRmHW3umEWpi4"},{"geometry":{"location":{"lat":32.968537,"lng":-97.04141}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"1822cefc17cc4b7e2ad2e4254862322f9b98adf7","name":"Paciugo","place_id":"ChIJFYwx7YYrTIYRvxmRPycKTLc"},{"geometry":{"location":{"lat":32.9661608,"lng":-97.042503}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png","id":"7af48219cb5dd760cfcaae2e722e5f8d27e6fb95","name":"Dippin' Dots","place_id":"ChIJFYwx7YYrTIYRO_1OD8RhGxo"}];
+console.log(JSONLocationData);
+var AppViewModel = function() {
+  //this.searchLoc = ko.observable("76262");
+  function Location(name, id, location, place_id) {
+    this.name = ko.observable(name);
+    this.id = ko.observable(id);
+    this.price = ko.observable(place_id);
+    this.latLong = ko.dependentObservable(function() {
+        return location.lat + "," + location.lng;
+    }, this);
+  }
+
+//do some basic mapping (without mapping plugin)
+var mappedData = ko.utils.arrayMap(JSONLocationData, function(loc) {
+   console.log(loc.geometry.location);
+    return new Location(loc.name, loc.id, loc.geometry.location, loc.place_id);
+});
+  //this.foundLoc = ko.computed(this.someData.formatted_address);
+  //this.Locations = ko.observableArray;
+  //var data = getCenter(this.searchLoc());
+  this.someData = ko.computed(function() {
+    getCenter(this.searchLoc());
+  }, this).extend({ async: true });
+  //this.foundLoc(getCenter(this.someData.formatted_address);
+  console.log(data);
+  //console.log(this.searchLoc());
+  //this.Locationsl = ko.mapping.fromJS(data);
+  //ko.mapping.fromJS(data, viewModel);
 //this.searchLoc = "76262";
 }
 
 // Activates knockout.js
 $(document).ready(function() {
   ko.applyBindings(new AppViewModel());
+  //var geocoder = new google.maps.Geocoder();
+  console.log(document);
 });
+
 
 
 // Create a map variable
@@ -14,7 +44,7 @@ var map;
      // Function to initialize the map within the map div
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.74135, lng: -73.99802},
+    center: {lat: 33.0040126, lng: -97.2258483},
     zoom: 14
   });
   // Create a single latLng literal object.
@@ -38,6 +68,19 @@ function initMap() {
         infowindow.close();
       });
     }
+  }
+  function textSearchPlaces() {
+    var bounds = map.getBounds();
+    hideMarkers(placeMarkers);
+    var placesService = new google.maps.places.PlacesService(map);
+    placesService.textSearch({
+      query: document.getElementById('places-search').value,
+      bounds: bounds
+    }, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        createMarkersForPlaces(results);
+      }
+    });
   }
   marker.addListener('click', function() {
     populateInfoWindow(this, infoWindow);
